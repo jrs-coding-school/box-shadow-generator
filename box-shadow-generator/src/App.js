fc6b6affdaae69bf9f3df3b3a51354b6b4233319
gotoworq <gotoworq@gmail.com>
2022-08-22 14:27:52 -0400

    changed App file

--- box-shadow-generator/src/App.js
+++ box-shadow-generator/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import BrowseDesignsPage from './components/BrowseDesignsPage/BrowseDesignsPage';
 
 function App() {
   return (
     <div className="app">
-      App
+      <BrowseDesignsPage />
     </div>
   );
 }
