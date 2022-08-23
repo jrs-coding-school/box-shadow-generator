ea3b04890e6d41ae983f2d285f06ba926f2ce72b
BlakeIngle <blake.ingle7@gmail.com>
2022-08-23 12:31:21 -0400

    refactored cssPreview html template

--- box-shadow-generator/src/components/CssPreview/CssPreview.js
+++ box-shadow-generator/src/components/CssPreview/CssPreview.js
@@ -3,21 +3,36 @@ import './CssPreview.css'
 
 export default function CssPreview({ x, y, blur, grow, inset, color }) {
 
+    const cssText = `.element {
+        box-shadow: ${x}px ${y}px ${blur}px ${grow}px {inset ? 'inset' : ''} #${color};
+        }`
     function handleCopyClicked() {
-        navigator.clipboard.writeText(`.element {
-            box-shadow: ${x}px ${y}px ${blur}px ${grow}px {inset ? 'inset' : ''} #${color};
-            }`);
+        navigator.clipboard.writeText(cssText);
+    }
+
+    const codeArr = getCodeLines(cssText);
+
+    function getCodeLines(text) {
+        let arr = [];
+
+        let cBraceIndex = text.indexOf('{')
+        arr.push(text.slice(0, cBraceIndex + 1));
+        let sColonIndex = text.indexOf(';')
+        arr.push(text.slice(cBraceIndex + 1, sColonIndex + 1));
+        arr.push(text.slice(sColonIndex + 1, text.length));
+
+        return arr;
     }
 
     return (
         <div className='css-preview-root'>
-            <code>
-                .element {'{'}
-            </code>
-            <code className='tab-1'>
-                box-shadow: {x}px {y}px {blur}px {grow}px{inset ? ' inset' : ''} #{color};
-            </code>
-            <code>{'}'}</code>
+            {codeArr.map((text, i) => (
+                <code key={i}
+                    className={(i == 0 || i == codeArr.length - 1) ? '' : 'tab-1'}
+                >
+                    {text}
+                </code>
+            ))}
 
             <button onClick={
                 handleCopyClicked
