e429028b514b899a29d4243ccd89f0df19f6421a
gotoworq <gotoworq@gmail.com>
2022-08-23 11:19:53 -0400

    Added functionality to BoxShadowPreview component

--- box-shadow-generator/src/components/BoxShadowGeneratorPage/BoxShadowGeneratorPage.js
+++ box-shadow-generator/src/components/BoxShadowGeneratorPage/BoxShadowGeneratorPage.js
@@ -1,16 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import BoxShadowControls from '../BoxShadowControls/BoxShadowControls'
 import BoxShadowPreview from '../BoxShadowPreview/BoxShadowPreview'
 import CssPreview from '../CssPreview/CssPreview'
 import './BoxShadowGeneratorPage.css'
 
 export default function BoxShadowGeneratorPage() {
+
+    const [shadow, setShadow] = useState({
+        id: '',
+        x: 5,
+        y: 5,
+        blur: 5,
+        grow: 5,
+        inset: false,
+        color: '000000cc'
+    })
+
+
+
     return (
+
         <div className="box-shadow-generator-root">
             <div className="top">
                 foo bar
             </div>
-            <BoxShadowPreview />
+            <BoxShadowPreview
+                {...shadow}
+            />
             <div className="bottom">
                 <BoxShadowControls />
                 <CssPreview />
--- box-shadow-generator/src/components/BoxShadowPreview/BoxShadowPreview.js
+++ box-shadow-generator/src/components/BoxShadowPreview/BoxShadowPreview.js
@@ -1,8 +1,28 @@
 import React from 'react'
 import './BoxShadowPreview.css'
 
-export default function BoxShadowPreview() {
+export default function BoxShadowPreview({ x, y, blur, grow, inset, color }) {
+
+
+    let style = {
+        boxShadow: `
+    ${x}px 
+    ${y}px 
+    ${blur}px 
+    ${grow}px 
+    ${inset ? 'inset' : ''} 
+    #${color}`
+    }
+
+
     return (
-        <div>BoxShadowPreview</div>
+
+
+
+        <div className='preview-root'>
+            <div className='preview-actual' style={style}>
+                I'm baby everyday carry celiac swag umami pabst butcher big mood hoodie, farm-to-table iPhone readymade. Cronut gatekeep hoodie shaman tbh readymade hot chicken woke chia thundercats ennui bespoke unicorn edison bulb four loko. XOXO migas lomo disrupt. Ugh synth sriracha kitsch Brooklyn forage chia godard +1 DIY YOLO bodega boys. Hammock meggings health goth fit twee, semiotics locavore truffaut cray leggings pork belly readymade shoreditch gluten-free crucifix. Edison bulb man bun enamel pin lo-fi heirloom jean shorts kitsch beard hashtag banjo hoodie yr fanny pack.
+            </div>
+        </div>
     )
 }
